f21d9e0cd20f12abafba6859db0c9b28d4d83981
mitya <mitya@adfox.ru>
2015-07-10 18:20:07 +0300

    sd

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 #!flask/bin/python
 from ig_app import app
+import os
+
 
 app.debug = True
-app.run(threaded=True, host='::', port=8080)
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
+# app.run(threaded=True, host='::', port=8080)
